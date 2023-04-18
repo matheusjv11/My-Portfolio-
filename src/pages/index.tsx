de7ab76d1862a07a507339778d87f01f6f513693
mfsilva <mfsilva@nelogica.com.br>
2023-04-18 20:52:12 -0300

    change icon to png

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <title>Matheus Almeida</title>
         <meta name="description" content="This is my personal website. Hope you enjoy it!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/faviconbetter.png" />
       </Head>
       <main className={styles.main}>
         <Header />
