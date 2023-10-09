a36973831ed93a24a706bf7c092b5a6f1da08cd8
mfsilva <mfsilva@nelogica.com.br>
2023-10-09 07:44:26 -0300

    Add background circle and some style fixing

--- src/pages/components/About/index.tsx
+++ src/pages/components/About/index.tsx
@@ -1,49 +1,56 @@
-import React from 'react'
-import { AboutSection } from "@/styles/about"
-import { experiences, hardSkills } from '@/services/about'
-import Paragraph from "@/pages/components/Base/Paragraph"
-import ExperienceCard from "../Base/ExperienceCard"
+import React from "react";
+import { AboutSection } from "@/styles/about";
+import { experiences, hardSkills } from "@/services/about";
+import Paragraph from "@/pages/components/Base/Paragraph";
+import ExperienceCard from "../Base/ExperienceCard";
 import Carousel from "react-elastic-carousel";
 
-const breakPoints = [
-    { width: 2, itemsToShow: 1 },
-  ];
+const breakPoints = [{ width: 2, itemsToShow: 1 }];
 
-export default function InitialPage () {
-    return(
-        <AboutSection id='about' className='home'>
-            <div className="myself__paragraph">
-                <Paragraph title="About Me" className="text">
-                    <p>
-                        Gratuated in 2021, in <strong>Computer Science</strong> at Federal University of Tocantins, I have been working as Software Developer since. Focused on <strong>Web Development </strong>
-                        since 2019, most of the time in Fullstack roles.
-                    </p>
-                    <p>
-                        Currently I live in <strong>Porto Alegre, Brazil</strong>. My family is spread throughout the country, which made me love being in contact with different cultures.  
-                    </p>
-                    <p>
-                        Besides loving music, movies, games and development, I could say that my favorite hobby is <strong>rollerboarding</strong>.    
-                    </p>
-                </Paragraph>
-                <Paragraph title="HardSkills">
-                    <div className="skill__container">
-                        {hardSkills.map((skill, i) => {
-                            return <span key={i} className="skill__badge">{skill}</span>
-                        })} 
-                    </div>
-                </Paragraph>
-            </div>
-            <div>
-                <Paragraph title="Work Experience">
-                    {/* 
+export default function InitialPage() {
+  return (
+    <AboutSection id="about" className="home">
+      <div className="myself__paragraph">
+        <Paragraph title="About Me" className="text">
+          <p>
+            Gratuated in 2021, in <strong>Computer Science</strong> at Federal
+            University of Tocantins, I have been working as Software Developer
+            since. Focused on <strong>Web Development </strong>
+            since 2019, most of the time in Fullstack roles.
+          </p>
+          <p>
+            Currently I live in <strong>Porto Alegre, Brazil</strong>. My family
+            is spread throughout the country, which made me love being in
+            contact with different cultures.
+          </p>
+          <p>
+            Besides loving music, movies, games and development, I could say
+            that my favorite hobby is <strong>rollerboarding</strong>.
+          </p>
+        </Paragraph>
+        <Paragraph title="HardSkills">
+          <div className="skill__container">
+            {hardSkills.map((skill, i) => {
+              return (
+                <span key={i} className="skill__badge">
+                  {skill}
+                </span>
+              );
+            })}
+          </div>
+        </Paragraph>
+      </div>
+      <div>
+        <Paragraph title="Work Experience">
+          {/* 
                     // @ts-ignore */}
-                    <Carousel breakPoints={breakPoints} itemPadding={[1, 5]} > 
-                        {experiences.map((xp, i) => (
-                            <ExperienceCard key={i} {...xp} />
-                        ))}
-                    </Carousel>
-                </Paragraph>
-            </div>
-        </AboutSection>
-    )
+          <Carousel breakPoints={breakPoints} itemPadding={[1, 5]}>
+            {experiences.map((xp, i) => (
+              <ExperienceCard key={i} {...xp} />
+            ))}
+          </Carousel>
+        </Paragraph>
+      </div>
+    </AboutSection>
+  );
 }
--- src/styles/initialPage.ts
+++ src/styles/initialPage.ts
@@ -1,47 +1,52 @@
-import styled from 'styled-components'
-import { Section } from '@/styles/base'
+import styled from "styled-components";
+import { Section } from "@/styles/base";
 
 export const Image = styled.img`
-    max-height: 400px;
+  max-height: 400px;
+  z-index: 2;
 
-    @media (max-width: 768px) {
-        max-height: 250px;
-    }
-`
+  @media (max-width: 768px) {
+    max-height: 35vh;
+  }
+`;
 
 export const InitialSection = styled(Section)`
-    display: flex;
-    color: white;
-    position: relative;
-    justify-content: center;
-    align-items: center;
-    padding: 6rem;
-    padding-bottom: 4rem;
-    flex-wrap: wrap;
-    gap: 3rem;
+  display: flex;
+  color: white;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  padding: 6rem;
+  padding-bottom: 4rem;
+  flex-wrap: wrap;
+  gap: 3rem;
 
-    @media (max-width: 768px) {
-        padding-top: 1.5rem;
-    }
+  @media (max-width: 768px) {
+    padding-top: 10vh;
+    gap: 0px;
+    align-items: flex-start;
+    padding: 4.8rem;
+  }
 `;
 
 export const PresentationText = styled.div`
-   display: flex;
-   flex-direction: column;
-   max-width: 550px;
-   color: var(--light-text);
-    
-    h1 {
-        margin-bottom: 1rem;
-        font-size: 3.5rem;
+  display: flex;
+  flex-direction: column;
+  max-width: 560px;
+  color: var(--light-text);
 
-        @media (max-width: 768px) {
-            font-size: 3rem;
-        }
-    }
+  h1 {
+    margin-bottom: 1rem;
+    font-size: clamp(1.8rem, calc(0.8 * 4.5vw), 4rem);
+  }
 
-    h3 {
-        margin-bottom: 2rem;
-        font-weight: 500;
-    }
-`
+  h3 {
+    font-size: clamp(1rem, calc(0.8 * 1.5vw), 4rem);
+    margin-bottom: 2rem;
+    font-weight: 500;
+  }
+
+  p {
+    max-width: 485px;
+  }
+`;
